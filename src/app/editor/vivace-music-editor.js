64a18580947f0597086c2195e38bc2b07bc3b246
Jason Chen <jasonschen5@gmail.com>
2014-09-23 00:07:28 -0700

    staff and trebleClef sign drawing

--- src/app/editor/vivace-music-editor.js
+++ src/app/editor/vivace-music-editor.js
@@ -10,144 +10,107 @@
 
     var Point = function (x, y) {
       this.x = x; this.y = y;
+
+      this.clone = function() {
+        return new Point(this.x, this.y);
+      };
     };
 
     var stage = new createjs.Stage(this.attr('id'));
     stage.canvas.width = $self.width();
 
-    ///* BEGIN DRAWER DEFINITIONS *///
+    ///* BEGIN OBJ DEFINITIONS *///
 
-    /* LineDrawer: Returns an object used to specify and draw lines */
-    var LineDrawer = function (p1, p2, stage, options) {
-      /// <params> p1 = start point object </params>
-      /// <params> p2 = end point object </params>
-      /// <params> stage = easel stage object with canvas </params>
-      /// <params> options = different properties for line
-      this._start = p1;
-      this._end = p2;
+    var Line = function (p1, p2, stage, options) {
+      this.start = p1;
+      this.end = p2;
+      this.stage = stage;
       this.options = options || {};
-      this._color = this.options.color || '#000';
-      this._thickness = this.options.thickness || 1;
+      this.color = this.options.color || '#000';
+      this.thickness = this.options.thickness || 1;
 
-      this._line = new createjs.Shape();
-      this._line.graphics.setStrokeStyle(this._thickness);
-
-      // caller must manually call endDraw() to finish drawing
-      this._line.graphics.beginStroke(this._color);
-
-      this.draw = function() {
-        this._line.graphics.moveTo(this._start.x, this._start.y).lineTo(this._end.x, this._end.y);
-      };
-
-      // ends stroke and returns createjs.Shape obj to be added to stage
-      this.endDraw = function() {
-        this._line.graphics.endStroke();
-        return this._line;
-      };
-
-      this.setStart = function(p) {
-        this._start.x = p.x || this._start.x;
-        this._start.y = p.y || this._start.y;
-      };
-
-      this.setEnd = function(p) {
-        this._end.x = p.x || this._end.x;
-        this._end.y = p.y || this._end.y;
-      };
-
-      this.incrementStart = function(delta) {
-        var dx = delta.x || 0;
-        var dy = delta.y || 0;
-        this._start.x += dx;
-        this._start.y += dy;
-      };
-
-      this.incrementEnd = function(delta) {
-        var dx = delta.x || 0;
-        var dy = delta.y || 0;
-        this._end.x += dx;
-        this._end.y += dy;
-      };
-
-      this.setColor = function(color) {
-        this._color = color;
-        this._line.graphics.beginStroke(color);
-      };
-
-      this.setThickness = function(thickness) {
-        this._thickness = thickness;
-        this._line.graphics.setStrokeStyle(thickness);
-      };
-    };
-    /* End LineDrawer Definition */
-
-    /* TrebleClef: Returns an obj used to spec and draw treble clef  */
-    var TrebleClefDrawer = function (color, topStartPt, bottomStartPt) {
-      this._color = color;
-      this._top = topStartPt;
-      this._bottom = bottomStartPt;
-      this._img = new Image();
-      this._img.src = 'assets/treble.png';
+      this.shape = new createjs.Shape();
 
       this.draw = function () {
-        var ready = $.Deferred();
-        this._img.onload = $.proxy(function (e) {
-          this._trebleClef = new createjs.Bitmap(e.target);
-          this._trebleClef.x = -10;
-          this._trebleClef.y = -22;
-          this._trebleClef.scaleX = 0.09;
-          this._trebleClef.scaleY = 0.09;
-          ready.resolve();
+        this.shape.graphics.setStrokeStyle(this.thickness).beginStroke(this.color);
+        this.shape.graphics.moveTo(this.start.x, this.start.y).lineTo(this.end.x, this.end.y);
+        this.shape.graphics.endStroke();
+        this.stage.addChild(this.shape);
+      };
+    };
+
+    var VivaceImage = function (pos, scale, color, src, stage) {
+      this.position = pos;
+      this.scale = scale;
+      this.color = color;
+      this.src = src;
+      this.stage = stage;
+      this._deferred = $.Deferred();
+      this.imgReady = this._deferred.promise();
+
+      this.init = function () {
+        var img = new Image();
+        img.src = this.src;
+        img.onload = $.proxy(function (e) {
+          this.image = new createjs.Bitmap(e.target);
+          this.image.x = this.position.x;
+          this.image.y = this.position.y;
+          this.image.scaleX = this.scale.x;
+          this.image.scaleY = this.scale.y;
+          this._deferred.resolve();
         }, this);
-        return ready.promise();
       };
-
-      this.endDraw = function () {
-        return this._trebleClef;
-      };
-    };
-
-    /* StaffDrawer: Returns an object used to spec and draw staff */
-    var StaffDrawer = function (startPoint, stage) {
-      this._start = startPoint;
-      this._stage = stage;
-
-      // p1, p2 will be updated per line of staff
-      this.p1 = new Point(this._start.x, this._start.y);
-      this.p2 = new Point(this._stage.canvas.width, this._start.y);
+      this.init();
 
       this.draw = function () {
-        var lineDrawer = new LineDrawer(this.p1, this.p2, this._stage, {
-          color: '#000',
-          thickness: 1
-        });
-        for (var i = 0; i < 5; i++) {
-          if (i !== 0) {
-            lineDrawer.incrementStart({ y: 20 });
-            lineDrawer.incrementEnd({ y: 20 });            
-          }
-          lineDrawer.draw();
-        }
-
-        this._stage.addChild(lineDrawer.endDraw());
-
-        var trebleClefDrawer = new TrebleClefDrawer('#000', this._start, this.p1);
-        trebleClefDrawer.draw().done(function () {
-          var treble = trebleClefDrawer.endDraw();
-          stage.addChild(treble);
-          stage.update();
-        });
+        this.imgReady.done($.proxy(function() {
+          this.stage.addChild(this.image);
+        }, this));
+        return this.imgReady;
       };
     };
-    /* End StaffDrawer Definition */
 
-    ///* END DRAWER DEFINITIONS *///
+    var Staff = function (topLeft, width, space, stage) {
+      this.topLeft = topLeft;
+      this.width = width;
+      this.space = space;
+      this.stage = stage;
+      this.components = [];
+
+      this.init = function () {
+        for (var i = 0; i < 5; i++) {
+          var p1 = new Point(topLeft.x, topLeft.y + i * this.space);
+          var p2 = new Point(p1.x + width, topLeft.y + i * this.space);
+          this.components.push(new Line(p1, p2, this.stage));
+        }
+        var trebleClefPos = new Point(this.topLeft.x - 10, this.topLeft.y - 37);
+        var trebleClefScl = { x: 0.09, y: 0.09 };
+        var trebleClef = new VivaceImage(trebleClefPos, trebleClefScl,
+                                         '#000', 'assets/treble.png', this.stage);
+        this.components.push(trebleClef);
+      };
+      this.init();
+
+      this.draw = function () {
+        var imagesReady = [];
+        for (var i = 0; i < this.components.length; i++) {
+          var comp = this.components[i];
+          if (comp instanceof VivaceImage) {
+            imagesReady.push(comp.draw());
+          } else {
+            comp.draw();
+          }
+        }
+        $.when.apply($, imagesReady).done($.proxy(function () {
+          this.stage.update();
+        }, this));
+      };
+    };
+    ///* END OBJ DEFINITIONS *///
 
     var topLeft = new Point(0, 15);
-    var staffDrawer = new StaffDrawer(topLeft, stage);
-    staffDrawer.draw();
-
-    stage.update();
+    var staff = new Staff(topLeft, stage.canvas.width, 20, stage);
+    staff.draw();
 
     return this;
 
